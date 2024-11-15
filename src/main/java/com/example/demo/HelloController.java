537f53382290dd9eead5655c0577bccb9a8422d2
lephuduy298 <leduyphucat@gmail.com>
2024-11-15 14:37:39 +0700

    new-update-chapter6

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,17 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello Spring Boot, Im Lê Phú Duy";
+        return "Hello Spring Boot, Im Lê Phú Duy - KMA";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "You're user of page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "You're admin of page";
     }
 
 }
