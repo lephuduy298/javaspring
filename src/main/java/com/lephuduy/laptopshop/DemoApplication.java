dacc39b53154148ed4b6c0d4d56cc92fb02da375
lephuduy298 <leduyphucat@gmail.com>
2024-11-17 13:33:44 +0700

    new-update-mvc

--- src/main/java/com/lephuduy/laptopshop/DemoApplication.java
+++ src/main/java/com/lephuduy/laptopshop/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.lephuduy.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
