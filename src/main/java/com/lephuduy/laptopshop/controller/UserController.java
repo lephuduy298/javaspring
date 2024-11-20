054b10cdce52039c26bc06579101374ea34379de
lephuduy298 <leduyphucat@gmail.com>
2024-11-20 21:36:47 +0700

    new-update-jsp

--- src/main/java/com/lephuduy/laptopshop/controller/UserController.java
+++ src/main/java/com/lephuduy/laptopshop/controller/UserController.java
@@ -8,16 +8,7 @@ import com.lephuduy.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello from Controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
 
     private UserService userService;
@@ -26,8 +17,24 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "hello";
     }
 }
+
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
