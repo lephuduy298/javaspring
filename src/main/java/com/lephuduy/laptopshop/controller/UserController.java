7ae2600abfb9d1a2e7f6e50c96dcb41d7ee98b3e
lephuduy298 <leduyphucat@gmail.com>
2024-11-23 01:48:28 +0700

    update-new

--- src/main/java/com/lephuduy/laptopshop/controller/UserController.java
+++ src/main/java/com/lephuduy/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.lephuduy.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,10 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        model.addAttribute("lephuduy", test);
+        model.addAttribute("hoidanit", "hello model from jsp");
         return "hello";
     }
 }
