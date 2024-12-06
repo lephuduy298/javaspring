c8e18a483e9897d39c262c0b288f2b21a637a267
lephuduy298 <leduyphucat@gmail.com>
2024-12-07 00:24:55 +0700

    update-mapping-relationship-data

--- src/main/java/com/lephuduy/laptopshop/domain/Order.java
+++ src/main/java/com/lephuduy/laptopshop/domain/Order.java
@@ -1,17 +1,31 @@
 package com.lephuduy.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "orders")
 public class Order {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private double totalPrice;
-    // user id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "order")
+    List<OrderDetail> orderDetails;
 
     public long getId() {
         return id;
@@ -33,5 +47,4 @@ public class Order {
     public String toString() {
         return "Order [id=" + id + ", totalPrice=" + totalPrice + "]";
     }
-
 }
--- src/main/java/com/lephuduy/laptopshop/domain/User.java
+++ src/main/java/com/lephuduy/laptopshop/domain/User.java
@@ -1,11 +1,18 @@
 package com.lephuduy.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     // 1 Attributes
     @Id
@@ -19,10 +26,17 @@ public class User {
     private String avatar;
     // roleId;
 
+    @ManyToOne
+    @JoinColumn(name = "rold_id")
+    private Role role;
+
     // getter, setter methods
     public long getId() {
         return this.id;
     }
+
+    @OneToMany(mappedBy = "user")
+    List<Order> order;
 
     public void setId(long id) {
         this.id = id;
