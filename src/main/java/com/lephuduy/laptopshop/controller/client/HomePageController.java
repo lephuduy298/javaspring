93a4e6cafe7605ccee9c9caf73ccf0038e97112b
lephuduy298 <leduyphucat@gmail.com>
2024-12-29 01:44:44 +0700

    update-dto-108

--- src/main/java/com/lephuduy/laptopshop/controller/client/HomePageController.java
+++ src/main/java/com/lephuduy/laptopshop/controller/client/HomePageController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.lephuduy.laptopshop.domain.Product;
+import com.lephuduy.laptopshop.domain.dto.RegisterDTO;
 import com.lephuduy.laptopshop.service.ProductService;
 
 @Controller
@@ -22,7 +25,18 @@ public class HomePageController {
     public String getHomePage(Model model) {
         List<Product> products = this.productService.fecthProducts();
         model.addAttribute("products", products);
-        return "/client/homepage/show";
+        return "client/homepage/show";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        model.addAttribute("registerUser", new RegisterDTO());
+        return "/client/auth/register";
+    }
+
+    @PostMapping("/register")
+    public String handleRegister(@ModelAttribute("registerUser") RegisterDTO registerDTO) {
+        return "/client/auth/register";
     }
 
 }
--- src/main/webapp/WEB-INF/view/client/homepage/show.jsp
+++ src/main/webapp/WEB-INF/view/client/homepage/show.jsp
@@ -69,8 +69,6 @@
 
                 <jsp:include page="../layout/banner.jsp" />
 
-                <jsp:include page="../layout/feature.jsp" />
-
                 <!-- Fruits Shop Start-->
                 <div class="container-fluid fruite py-5">
                     <div class="container py-5">
@@ -133,6 +131,9 @@
                         </div>
                     </div>
                     <!-- Fruits Shop End-->
+
+                    <jsp:include page="../layout/feature.jsp" />
+
 
                     <jsp:include page="../layout/footer.jsp" />
 
